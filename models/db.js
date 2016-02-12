d4252f6b7380a8b945d97f0b74c4891ba5abf583
Ferrizal <ferrizal@mataharimall.com>
2016-02-12 18:27:06 +0700

    Environment variables to use .env

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
-var env = require('../env');
+require('dotenv').config();
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : process.env['DB_HOST'],
-    user     : process.env['DB_USER'],
-    password : process.env['DB_PASSWORD'],
-    database : process.env['DB_NAME']
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 });
 
 connection.connect();
